e58cd0f0e12a1fdbbdb816c6b0f3c14d806724ac
Emil Lehti <29.lehti@gmail.com>
2021-05-03 07:42:50 +0200

    More stuff

--- content.js
+++ content.js
@@ -1,34 +1,43 @@
 document.getElementById('calc').addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  function setPageBackgroundColor() {
-      var rows = document.querySelectorAll('#intervals-table .active');
-      var footer = document.querySelector('#intervals-table table tfoot');
-      var f = footer.cloneNode(true);
-    var total;
-    console.log(f);
-    console.log(rows);
-    rows.forEach(row => {
-        row.cells.forEach(cell => total[cell.cellIndex] += toSeconds(cell.innerText))
-    });
-    //   console.log(rows);
-    //   console.log(rows.length);
-  }
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  function toSeconds(duration) {
-    var t = duration.split(':');
-    var sec;
-    if (t.length === 3) { //hh:mm:s
-        t = t.map(x => parseInt(x, 10));
-        sec = (t[0] * 60*60) + (t[1]*60) + t2;
-    } else if (t.length === 2) { //mm:s
-        t = t.map(x => parseInt(x, 10));
-        sec = t[0] * 60 + t[1]:
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: calcAverage,
+  });
+});
+
+
+// function toSeconds(duration) {
+//   var t = duration.split(':');
+//   var sec;
+//   if (t.length === 3) { //hh:mm:s
+//     t = t.map(x => parseInt(x, 10));
+//     sec = (t[0] * 60 * 60) + (t[1] * 60) + t2;
+//   } else if (t.length === 2) { //mm:s
+//     t = t.map(x => parseInt(x, 10));
+//     sec = t[0] * 60 + t[1];
+//   }
+// }
+
+// function toMinutes(avgSeconds) {
+//   const seconds = avgSeconds % 60;
+//   const minutes = Math.floor(avgSeconds / 60);
+//   return minutes.toString() + ':' + seconds.toString();
+// }
+
+function calcAverage() {
+  var rows = document.querySelectorAll('#intervals-table .active');
+  var footer = document.querySelector('#intervals-table table tfoot');
+  var f = footer.cloneNode(true);
+  var total = [];
+  rows.forEach(row => {
+    for (let i = 0; i < row.children.length; i++) {
+      var p = row.children[i].innerHTML.trim();
+      total[4] += helpers.toSeconds(p);
     }
-  }
+  });
+  var avgSeconds = total[4] / rows.length;
+  var avgPace = helpers.toMinutes(avgSeconds);
+  console.log('Average Pace is: ' + avgPace);
+}
---
+++ helpers.js
@@ -0,0 +1,29 @@
+var helpers = (function () {
+    var toSeconds = function (duration) {
+        var t = duration.split(':');
+        var sec;
+        if (t.length === 3) { //hh:mm:s
+            t = t.map(x => parseInt(x, 10));
+            sec = (t[0] * 60 * 60) + (t[1] * 60) + t2;
+        } else if (t.length === 2) { //mm:s
+            t = t.map(x => parseInt(x, 10));
+            sec = t[0] * 60 + t[1];
+        }
+    }
+
+    var toMinutes = function (avgSeconds) {
+        const seconds = avgSeconds % 60;
+        const minutes = Math.floor(avgSeconds / 60);
+        return minutes.toString() + ':' + seconds.toString();
+    }
+    // var getRandomArbitrary = function(min, max) {
+    //   return Math.floor(Math.random() * (max - min)) + min;
+    // }
+    // return {
+    //   getRandomArbitrary: getRandomArbitrary
+    // }
+    return {
+        toSeconds: toSeconds,
+        toMinutes: toMinutes
+    }
+})()
