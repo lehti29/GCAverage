d5f93861a41d858a4fce9cab455b12020ba1829d
Emil Lehti <29.lehti@gmail.com>
2021-08-20 20:51:18 +0200

    More

--- content.js
+++ content.js
@@ -9,41 +9,54 @@ document.getElementById('calc').addEventListener("click", async () => {
 
 function calcAverage() {
   var rows = document.querySelectorAll('#intervals-table .active');
-  var footer = document.querySelector('#intervals-table table tfoot');
   var table = document.querySelector('#intervals-table table');
-  var f = footer.cloneNode(true);
-  var total = [];
-  var tutti = 0;
+  var total = new Array(10);
+  total[1] = total[3] = total[4] = 0;
   rows.forEach(row => {
     for (let i = 0; i < row.children.length; i++) {
-      if (i != 4) continue;
-      var p = row.children[i].innerHTML.trim();
-      var t = p.split(':');
-      var sec;
-      if (t.length === 3) { //hh:mm:s
-        t = t.map(x => parseInt(x, 10));
-        sec = (t[0] * 60 * 60) + (t[1] * 60) + t2;
-      } else if (t.length === 2) { //mm:s
-        t = t.map(x => parseInt(x, 10));
-        sec = t[0] * 60 + t[1];
+      if (i == 4) {
+        var p = row.children[i].innerHTML.trim();
+        var timeSplit = p.split(':');
+        var sec = getSec(timeSplit);
+        total[i] += sec;
+      } else if (i == 1) {
+        var p = row.children[i].innerHTML.trim();
+        var timeSplit = p.split(':');
+        var sec = getSec(timeSplit);
+        total[i] += sec;
       }
-      total[4] += sec;
-      tutti = tutti + sec;
     }
   });
-  avgSeconds = tutti / rows.length;
-
-  const seconds = avgSeconds % 60;
-  const minutes = Math.floor(avgSeconds / 60);
-  var avgPace = minutes.toString() + ':' + seconds.toFixed(1);
+  var avg1 = toTime(total[1], rows.length);
+  var avg4 = toTime(total[4], rows.length);
   
   if (document.querySelectorAll('#intervals-table table tfoot').length <= 1) {
-    table.appendChild(f);
-    var tr = f.firstChild;
+    var avgFooter = document.querySelector('#intervals-table table tfoot').cloneNode(true);
+    table.appendChild(avgFooter);
+    var tr = avgFooter.firstChild;
     for (var i = 0; i < tr.children.length; i++) {
       tr.children[i].innerHTML = "";
     }
     tr.children[0].innerHTML = "Average";
-    tr.children[4].innerHTML = avgPace;
+    tr.children[1].innerHTML = avg1;
+    tr.children[4].innerHTML = avg4;
+  }
+
+  function getSec(t){
+    if (t.length === 3) { //hh:mm:s
+      t = t.map(x => parseFloat(x, 10));
+      sec = (t[0] * 60 * 60) + (t[1] * 60) + t2;
+    } else if (t.length === 2) { //mm:s
+      t = t.map(x => parseFloat(x, 10));
+      sec = t[0] * 60 + t[1];
+    }
+    return sec;
+  }
+
+  function toTime(tutti, amount) {
+    avgSeconds = tutti / amount;
+    const seconds = avgSeconds % 60;
+    const minutes = Math.floor(avgSeconds / 60);
+    return minutes.toString() + ':' + seconds.toFixed(1);
   }
 }
