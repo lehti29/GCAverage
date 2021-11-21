e82a2287aae1f0249cedc3a3c04e9dd30388b71e
Emil Lehti <29.lehti@gmail.com>
2021-11-21 13:50:34 +0100

    Remove unused stuff

--- background.js
+++ background.js
@@ -1,19 +1,3 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     console.log('installed');
-// });
-
-// chrome.runtime.onMessage.addListener(({ type, name }) => {
-//     if (type === "set-name") {
-//         chrome.storage.local.set({ name });
-//     }
-// });
-
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.storage.local.get(["name"], ({ name }) => {
-//         chrome.tabs.sendMessage(tab.id, { name });
-//     });
-// });
-
 chrome.browserAction.onClicked.addListener(
     function (tab) {
         chrome.tabs.executeScript(tab.id, {
